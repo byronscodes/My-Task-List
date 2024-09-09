f00560694aa7a29b70c120b3ecb0d0a574021d60
Byron Streit <streitb2020@gmail.com>
2024-09-09 18:30:53 -0400

    Primary app function and readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+class App extends Component{
+  
+constructor(props){
+  super(props);
+  this.state={
+    notes:[]
+  }
+}
 
-function App() {
+API_URL = "http://localhost:5178/";
+
+componentDidMount(){
+  this.refreshNotes();
+}
+
+async refreshNotes(){
+  fetch(this.API_URL + "api/todoapp/GetNotes").then(response=>response.json())
+  .then(data=>{
+    this.setState({notes:data});
+  })
+}
+
+async addClick(){
+  var newNotes = document.getElementById("newNotes").value;
+  const data = new FormData();
+  data.append("newNotes", newNotes);
+
+  fetch(this.API_URL + "api/todoapp/AddNotes", {
+    method: "POST",
+    body: data
+  }).then(res=>res.json())
+  .then((result)=>{
+    alert(result);
+    this.refreshNotes();
+  })
+}
+
+async deleteClick(id){
+  fetch(this.API_URL + "api/todoapp/DeleteNotes?id=" + id, {
+    method: "DELETE",
+  }).then(res=>res.json())
+  .then((result)=>{
+    alert(result);
+    this.refreshNotes();
+  })
+}
+
+// custom components
+// import CustomForm from './components/CustomForm'
+
+render() {
+  const{notes} = this.state;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>My Task List</h1>
       </header>
+      <input id="newNotes"/>&nbsp;
+      <button onClick={()=>this.addClick()}>+</button>
+      {notes.map(note=>
+      <p>
+        <b>* {note.description}</b>&nbsp;
+        <button onClick={()=>this.deleteClick(note.id)}>✓</button>
+      </p>
+      )}
     </div>
   );
 }
+}
+
 
 export default App;
